50b1da1f3edc10c1bb1bd739bae50bc767113348
Abdul Ahad <abdulahad.momin07@gmail.com>
2019-12-26 01:02:08 +0530

    added FCM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { FCM } from '@ionic-native/fcm/ngx'
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,15 +16,33 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private fcm: FCM,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.styleLightContent();
       this.splashScreen.hide();
+
+      this.fcm.getToken().then(token => {
+        // send this token to the server
+        console.log(token)
+      })
+
+      this.fcm.onNotification().subscribe(data => {
+        console.log(data);
+        if (data.wasTapped) {
+          console.log('Received in background', data);
+          this.router.navigate(['home', {'from': data.fcm_from}]);
+        } else {
+          console.log('Received in foreground', data);
+          this.router.navigate(['home', {'from': data.fcm_from}]);
+        }
+      });
     });
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  from: string = ''
+
+  constructor(
+    private route: ActivatedRoute
+  ) {
+    this.from = this.route.snapshot.params['from']
+  }
 
 }
